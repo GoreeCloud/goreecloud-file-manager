f15b41ffae8a72e54e4530dd34c8caa4bb71918b
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-09 21:15:23 -0500

    Add Compose Multiplatform desktop build plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     id("com.android.application") version "9.3.0" apply false
     id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+    id("org.jetbrains.compose") version "1.12.0" apply false
 }
